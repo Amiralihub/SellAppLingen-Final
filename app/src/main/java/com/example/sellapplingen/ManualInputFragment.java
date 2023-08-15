9998a8c45ba1bb80b6ec0591551606aa2d4b3c1f
FrecherFruchtzwerg <sadik-bajrami@gmx.de>
2023-08-15 17:59:34 +0200

    Manuelle Order Information + PopUp für Bestellungs ID

---
+++ app/src/main/java/com/example/sellapplingen/ManualInputFragment.java
@@ -0,0 +1,85 @@
+package com.example.sellapplingen;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.sellapplingen.databinding.FragmentManualInputBinding;
+
+public class ManualInputFragment extends Fragment {
+
+    private FragmentManualInputBinding binding;
+    private Order currentOrder;
+
+    private String selectedZipCode = "";
+
+
+    public ManualInputFragment() {
+        // Required empty public constructor
+    }
+
+    public void setCurrentOrder(Order order) {
+        currentOrder = order;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentManualInputBinding.inflate(inflater, container, false);
+        setupViews();
+        return binding.getRoot();
+    }
+
+    private void setupViews() {
+        binding.confirmManualInputButton.setOnClickListener(v -> saveManualInput());
+        binding.zipButton.setOnClickListener(v -> showZipCodeOptions());
+    }
+
+
+    private void saveManualInput() {
+        // Speichere die manuell eingegebenen Order-Informationen im currentOrder-Objekt
+        currentOrder.setLastName(binding.lastNameEditText.getText().toString());
+        currentOrder.setFirstName(binding.firstNameEditText.getText().toString());
+        currentOrder.setStreet(binding.streetEditText.getText().toString());
+        currentOrder.setHouseNumber(binding.houseNumberEditText.getText().toString());
+        currentOrder.setZip(selectedZipCode);
+        currentOrder.setCity(binding.cityEditText.getText().toString());
+
+        // Erstelle das Bundle für die Übergabe der Order-Daten
+        Bundle args = new Bundle();
+        args.putSerializable("order", currentOrder);
+
+        // Erstelle das HandlingInfoFragment und setze die Argumente
+        HandlingInfoFragment handlingInfoFragment = new HandlingInfoFragment();
+        handlingInfoFragment.setArguments(args);
+
+        // Wechsle zum HandlingInfoFragment
+        FragmentManager fragmentManager = requireFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, handlingInfoFragment, "handlingInfoFragment");
+        transaction.addToBackStack(null); // Füge das Fragment zur Rückwärtsnavigation hinzu
+        transaction.commit();
+    }
+
+    private void showZipCodeOptions() {
+        // Erstelle ein Array mit den verfügbaren PLZ-Optionen
+        String[] zipCodes = {"49808", "49809", "49811"};
+
+        // Erstelle einen AlertDialog, um die PLZ-Optionen anzuzeigen
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Postleitzahl auswählen")
+                .setItems(zipCodes, (dialog, which) -> {
+                    // Wenn eine Option ausgewählt wurde, setze den ausgewählten Wert im Button-Text
+                    String selectedZip = zipCodes[which];
+                    binding.zipButton.setText(selectedZip);
+                    selectedZipCode = selectedZip; // Aktualisiere die ausgewählte Postleitzahl
+                });
+
+        // Zeige den AlertDialog
+        builder.create().show();
+    }
+
+
+}
