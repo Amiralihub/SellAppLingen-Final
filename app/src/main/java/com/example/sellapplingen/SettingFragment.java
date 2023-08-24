d05ec5d0001a766c0d13479ce222d45cb0c15489
amirali <amiraligng@yahoo.com>
2023-08-24 11:00:23 +0200

    isInputValid + Bug fixed

--- app/src/main/java/com/example/sellapplingen/SettingFragment.java
+++ app/src/main/java/com/example/sellapplingen/SettingFragment.java
@@ -77,7 +77,7 @@ public class SettingFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 isEditMode = true; // Aktivieren Sie den Bearbeitungsmodus
-                enableEditMode(); // Aktivieren Sie die Bearbeitung der Daten
+                enableEditMode(true); // Aktivieren Sie die Bearbeitung der Daten
             }
         });
 
@@ -157,19 +157,16 @@ public class SettingFragment extends Fragment {
     }
 
 
-    public void enableEditMode() {
-        // Aktivieren Sie die Bearbeitung der Daten
-        if (isEditMode) {
-            editStoreName.setEnabled(true);
-            editOwner.setEnabled(true);
-            editStreet.setEnabled(true);
-            editHouseNumber.setEnabled(true);
-            editZip.setEnabled(true);
-            editTelephone.setEnabled(true);
-            editEmail.setEnabled(true);
-        }
-
+    private void enableEditMode(boolean enable) {
+        editStoreName.setEnabled(enable);
+        editOwner.setEnabled(enable);
+        editStreet.setEnabled(enable);
+        editHouseNumber.setEnabled(enable);
+        editZip.setEnabled(enable);
+        editTelephone.setEnabled(enable);
+        editEmail.setEnabled(enable);
     }
+
 
     private void enableSaveButton() {
         if (isEditMode) {
@@ -217,146 +214,83 @@ public class SettingFragment extends Fragment {
         requireActivity().finish();
     }
 
-    private boolean isInputValid(String storeName, String owner, String street, String houseNumber, String zip, String telephone, String email) {
-        boolean isValid = true;
 
-        if (storeName.isEmpty()) {
-            isValid = false;
-            editStoreName.setError("Bitte geben Sie einen Geschäftsnamen ein");
-        }
 
-        if (owner.isEmpty()) {
-            isValid = false;
-            editOwner.setError("Bitte geben Sie den Eigentümer ein");
-        }
+    public enum ZipCode {
+        ZIP_40808("49808"),
+        ZIP_49809("49809"),
+        ZIP_49811("49811");
 
-        if (street.isEmpty()) {
-            isValid = false;
-            editStreet.setError("Bitte geben Sie eine Straße ein");
+        private final String value;
+
+        ZipCode(String value) {
+            this.value = value;
         }
 
-        if (houseNumber.isEmpty()) {
-            isValid = false;
-            editHouseNumber.setError("Bitte geben Sie eine Hausnummer ein");
+        public String getValue() {
+            return value;
         }
-        if (zip.isEmpty()) {
-            isValid = false;
-            editZip.setError("Bitte geben Sie einen Geschäftsnamen ein");
-        }
-        if (telephone.isEmpty()) {
-            isValid = false;
-            editTelephone.setError("Bitte geben Sie einen Geschäftsnamen ein");
-        }
-        if (email.isEmpty()) {
-            isValid = false;
-            editEmail.setError("Bitte geben Sie einen Geschäftsnamen ein");
-        }
-
-        return isValid;
     }
 
 
-//TODO
-
-/*
 
 
     private boolean isInputValid(String storeName, String owner, String street, String houseNumber, String zip, String telephone, String email) {
         boolean isValid = true;
 
-        if (storeName.isEmpty()) {
+        if (storeName.trim().isEmpty() || containsEmojis(storeName)) {
             isValid = false;
-            editStoreName.setError("Bitte geben Sie einen Geschäftsnamen ein");
+            editStoreName.setError("Bitte geben Sie einen gültigen Geschäftsnamen ein");
         }
 
-        if (owner.isEmpty()) {
+        if (owner.trim().isEmpty() || containsEmojis(owner)) {
             isValid = false;
-            editOwner.setError("Bitte geben Sie den Eigentümer ein");
+            editOwner.setError("Bitte geben Sie einen gültigen Eigentümer ein");
         }
 
-        if (street.isEmpty()) {
+        if (street.trim().isEmpty() || containsEmojis(street)) {
             isValid = false;
-            editStreet.setError("Bitte geben Sie eine Straße ein");
+            editStreet.setError("Bitte geben Sie eine gültige Straße ein");
         }
 
-        if (houseNumber.isEmpty()) {
+        if (houseNumber.trim().isEmpty() || containsEmojis(houseNumber)) {
             isValid = false;
-            editHouseNumber.setError("Bitte geben Sie eine Hausnummer ein");
+            editHouseNumber.setError("Bitte geben Sie eine gültige Hausnummer ein");
         }
 
-        if (zip.isEmpty()) {
+        if (!isValidZipCode(zip)) {
             isValid = false;
-            editZip.setError("Bitte geben Sie eine Postleitzahl ein");
+            editZip.setError("Bitte geben Sie eine gültige PLZ ein (49808,49809,49811)");
         }
 
-        if (telephone.isEmpty()) {
+        if (telephone.trim().isEmpty() || containsEmojis(telephone)) {
             isValid = false;
-            editTelephone.setError("Bitte geben Sie eine Telefonnummer ein");
+            editTelephone.setError("Bitte geben Sie eine gültige Telefonnummer ein");
         }
 
-        if (email.isEmpty()) {
+        if (email.trim().isEmpty() || containsEmojis(email)) {
             isValid = false;
-            editEmail.setError("Bitte geben Sie eine E-Mail-Adresse ein");
+            editEmail.setError("Bitte geben Sie eine gültige E-Mail-Adresse ein");
         }
 
         return isValid;
     }
 
-    // Verwende die Methode in deiner showConfirmationDialog-Methode
-    private void showConfirmationDialog() {
-        if (!isInputValid(
-                editStoreName.getText().toString(),
-                editOwner.getText().toString(),
-                editStreet.getText().toString(),
-                editHouseNumber.getText().toString(),
-                editZip.getText().toString(),
-                editTelephone.getText().toString(),
-                editEmail.getText().toString()
-        )) {
-            return;
+    private boolean isValidZipCode(String zip) {
+        for (ZipCode validZip : ZipCode.values()) {
+            if (validZip.getValue().equals(zip)) {
+                return true;
+            }
         }
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-        builder.setTitle("Daten speichern");
-        builder.setMessage("Sind Sie sicher, dass Sie die Daten speichern möchten?");
-
-        builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        setSettings(SettingParameter.storeName, editStoreName.getText().toString());
-                        setSettings(SettingParameter.owner, editOwner.getText().toString());
-                        setSettings(SettingParameter.telephone, editTelephone.getText().toString());
-                        setSettings(SettingParameter.email, editEmail.getText().toString());
-                        setAddress(editStreet.getText().toString(), editHouseNumber.getText().toString(), editZip.getText().toString());
-
-                        requireActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                saveData.setEnabled(false);
-                            }
-                        });
-                    }
-                }).start();
-                dialog.dismiss();
-            }
-        });
-
-        builder.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        return false;
     }
 
 
-*/
+    private boolean containsEmojis(String text) {
+        String regex = "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{1FA70}-\\x{1FAFF}\\x{2600}-\\x{27BF}\\x{2300}\\x{2B50}\\x{1F004}\\x{2B05}\\x{2744}\\x{26A1}\\x{1F004}\\x{1F0CF}\\x{1F18E}\\x{1F201}\\x{1F202}\\x{1F21A}\\x{1F22F}\\x{1F232}\\x{1F233}\\x{1F234}\\x{1F235}\\x{1F236}\\x{1F237}\\x{1F238}\\x{1F239}\\x{1F23A}\\x{1F250}\\x{1F251}\\x{1F300}\\x{1F301}\\x{1F302}\\x{1F303}\\x{1F304}\\x{1F305}\\x{1F306}\\x{1F307}\\x{1F308}\\x{1F309}\\x{1F30A}\\x{1F30B}\\x{1F30C}\\x{1F30D}\\x{1F30E}\\x{1F30F}\\x{1F310}\\x{1F311}\\x{1F312}\\x{1F313}\\x{1F314}\\x{1F315}\\x{1F316}\\x{1F317}\\x{1F318}\\x{1F319}\\x{1F31A}\\x{1F31B}\\x{1F31C}\\x{1F31D}\\x{1F31E}\\x{1F31F}\\x{1F320}\\x{1F321}\\x{1F322}\\x{1F323}\\x{1F324}\\x{1F325}\\x{1F326}\\x{1F327}\\x{1F328}\\x{1F329}\\x{1F32A}\\x{1F32B}\\x{1F32C}\\x{1F32D}\\x{1F32E}\\x{1F32F}\\x{1F330}\\x{1F331}\\x{1F332}\\x{1F333}\\x{1F334}\\x{1F335}\\x{1F336}\\x{1F337}\\x{1F338}\\x{1F339}\\x{1F33A}\\x{1F33B}\\x{1F33C}\\x{1F33D}\\x{1F33E}\\x{1F33F}\\x{1F340}\\x{1F341}\\x{1F342}\\x{1F343}\\x{1F344}\\x{1F345}\\x{1F346}\\x{1F347}\\x{1F348}\\x{1F349}\\x{1F34A}\\x{1F34B}\\x{1F34C}\\x{1F34D}\\x{1F34E}\\x{1F34F}\\x{1F350}\\x{1F351}\\x{1F352}\\x{1F353}\\x{1F354}\\x{1F355}\\x{1F356}\\x{1F357}\\x{1F358}\\x{1F359}\\x{1F35A}\\x{1F35B}\\x{1F35C}\\x{1F35D}\\x{1F35E}\\x{1F35F}\\x{1F360}\\x{1F361}\\x{1F362}\\x{1F363}\\x{1F364}\\x{1F365}\\x{1F366}\\x{1F367}\\x{1F368}\\x{1F369}\\x{1F36A}\\x{1F36B}\\x{1F36C}\\x{1F36D}\\x{1F36E}\\x{1F36F}\\x{1F370}\\x{1F371}\\x{1F372}\\x{1F373}\\x{1F374}\\x{1F375}\\x{1F376}\\x{1F377}\\x{1F378}\\x{1F379}\\x{1F37A}\\x{1F37B}\\x{1F37C}\\x{1F37D}\\x{1F37E}\\x{1F37F}\\x{1F380}\\x{1F381}\\x{1F382}\\x{1F383}\\x{1F384}\\x{1F385}\\x{1F386}\\x{1F387}\\x{1F388}\\x{1F389}\\x{1F38A}\\x{1F38B}\\x{1F38C}]";
+
+        return text.matches(regex);
+    }
 
 
     private void showConfirmationDialog() {
@@ -390,13 +324,14 @@ public class SettingFragment extends Fragment {
                                 @Override
                                 public void run() {
                                     saveData.setEnabled(false);
+                                    enableEditMode(false);
+
                                 }
                             });
                         }
                     }).start();
                     dialog.dismiss();
                 } else {
-                    // Zeige eine Fehlermeldung an, wenn die Validierung fehlschlägt
                     Toast.makeText(requireContext(), "Bitte überprüfen Sie die Eingabe", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -412,6 +347,7 @@ public class SettingFragment extends Fragment {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
 
 
 
@@ -528,7 +464,7 @@ public class SettingFragment extends Fragment {
             if (responseCode == 200) {
 
                 showSuccessPopup();
-                enableEditMode();
+
 
             } else {
                 showErrorPopup();
@@ -600,9 +536,9 @@ public class SettingFragment extends Fragment {
         requireActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                // Show the success message using a Toast on the main UI thread
+
                 Toast.makeText(requireContext(), "Daten erfolgreich an den Server gesendet.", Toast.LENGTH_SHORT).show();
-                enableEditMode();
+
             }
         });
     }
@@ -611,7 +547,6 @@ public class SettingFragment extends Fragment {
         requireActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                // Show the error message using a Toast on the main UI thread
                 Toast.makeText(requireContext(), "Keine Verbindung zum Server.", Toast.LENGTH_SHORT).show();
             }
         });
