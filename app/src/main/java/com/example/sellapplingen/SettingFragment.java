9b318f24bf0a03f51703dc2fab1f07cb4568d315
amirali <amiraligng@yahoo.com>
2023-08-24 12:49:30 +0200

    CleanUp

--- app/src/main/java/com/example/sellapplingen/SettingFragment.java
+++ app/src/main/java/com/example/sellapplingen/SettingFragment.java
@@ -18,7 +18,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.fragment.app.Fragment;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
